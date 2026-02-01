8f1e44e3602a0673ee6a589d4ff9b62569ba0848
davdabrinda <davdabrinda@gmail.com>
2026-02-01 19:59:03 +0530

    added imports of statss bar and navigation bard which are missing

--- app/src/main/java/com/appstudio/finmarka/ui/screens/addtransaction/AddEditTransactionScreen.kt
+++ app/src/main/java/com/appstudio/finmarka/ui/screens/addtransaction/AddEditTransactionScreen.kt
@@ -12,7 +12,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
--- app/src/main/java/com/appstudio/finmarka/ui/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/appstudio/finmarka/ui/screens/dashboard/DashboardScreen.kt
@@ -14,8 +14,11 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
