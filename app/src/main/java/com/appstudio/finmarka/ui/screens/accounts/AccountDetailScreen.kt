74c10d5cad0f97d36cfc5ba84b04464b702bb903
davdabrinda <davdabrinda@gmail.com>
2026-02-04 23:29:50 +0530

    added some imports

--- app/src/main/java/com/appstudio/finmarka/ui/screens/accounts/AccountDetailScreen.kt
+++ app/src/main/java/com/appstudio/finmarka/ui/screens/accounts/AccountDetailScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.background
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -40,6 +41,7 @@ import com.appstudio.finmarka.ui.util.formatCurrency
 import com.appstudio.finmarka.ui.util.formatDateShort
 import com.appstudio.finmarka.ui.viewmodel.AccountDetailViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AccountDetailScreen(
     accountId: Int,
